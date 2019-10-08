cec3334782d9a8248a94a052cda8123941200ec3
tparkeen <tparkeen@ucsc.edu>
2019-10-07 17:21:38 -0700

    Added comments

--- RecursiveRaytracer/src/rraytracer.cpp
+++ RecursiveRaytracer/src/rraytracer.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 int main()
 {
+    // Constants
     const uint32_t HEIGHT = 500;
     const uint32_t WIDTH = 500;
     const uint32_t RGB_MAX = 255;
@@ -21,7 +22,13 @@ int main()
     ofstream outputImage ("output.ppm");
     if (outputImage.is_open())
     {
+        /*  
+         *  "P3" means this is a RGB color image in ASCII
+         *  "WIDTH HEIGHT" is the image's width and height
+         *  "RBG_MAX" is the maximum value for each color
+         */
         outputImage << "P3\n" << WIDTH << " " << HEIGHT << "\n" << RGB_MAX << "\n";
+        // For each pixel, calculate the color using the raytracer
         for(size_t h = 0; h < HEIGHT; h++)
         {
             for(size_t w = 0; w < WIDTH; w++)
